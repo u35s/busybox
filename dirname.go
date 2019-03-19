c81289a0d3ed10c28ada3dd2e48a6704ad7cc333
zjt <jintao7080@gmail.com>
2019-03-19 11:41:39 +0800

    add dirname

---
+++ dirname.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func (app *applets) Applet_dirname(args []string) {
+	if len(args) < 2 {
+		fmt.Println("usage: dirname path")
+		os.Exit(1)
+	}
+	dir := args[1]
+	if len(dir) > 1 {
+		dir = strings.TrimSuffix(args[1], "/")
+	}
+	fmt.Println(filepath.Dir(dir))
+}
